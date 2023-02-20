20a2f9f60b30ce03b9cade4ded65a83506acce9a
Harry <staleyh@gmail.com>
2023-02-20 01:03:03 -0600

    Added gitignore and gitattributes

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize') // added dependency
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION OLD
+// const sequelize = new Sequelize(process.env.PG_URI)
+
+// SEQUELIZE CONNECTION
+ let sequelize = new Sequelize({ 
+    username: "postgres",
+    password: "",
+    host: "127.0.0.1",
+    dialect: "postgres"})
 
 // ROOT
 app.get('/', (req, res) => {
